7d256e9e1831cffbd5ad9a029bab076f39a48eec
VIP <belevitin2010@mail.ru>
2022-03-16 14:41:26 +0300

    fixed Metod return

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -2,29 +2,20 @@ package main.java;
 
 public class Start {
     public static void main(String[] args) {
-        // Ниже вызовите новый метод
-        String highestGrossingFilm = findHighestGrossingFilm();
+
+        final String highestGrossingFilm = findHighestGrossingFilm();
         System.out.println("Самый кассовый фильм: " + highestGrossingFilm);
     }
+
     public static String findHighestGrossingFilm() {
-        /** 
-        * если заранее известно что будет лежать в переменной, дай ей осмысленное название
-        * например titanicFilm так же и с переменной income1 -> titanicIncome
-        * и если значение изменяться не будет, сделай ее final
-        * https://vertex-academy.com/tutorials/ru/modifikator-final/
-        * так же и с остальными переменными
-        */
-        String titanicFilm = "Титаник";
-       final int titanicIncome = 2194;
-int a;
-        String AvatarFilm = "Аватар";
-       final int AvatarIncome = 2810;
+        final String titanicFilm = "Титаник";
+        final int titanicIncome = 2194;
+        final String AvatarFilm = "Аватар";
+        final int AvatarIncome = 2810;
+        final String DarkKnightFilm = "Тёмный рыцарь";
+        final int DarkKnightIncome = 1084;
 
-        String DarkKnightFilm = "Тёмный рыцарь";
-       final int DarkKnightIncome = 1084;
-
-        // Напишите реализацию нового метода ниже
-        if (titanicIncome >AvatarIncome) {
+        if (titanicIncome > AvatarIncome) {
             if (AvatarIncome > DarkKnightIncome) {
                 return titanicFilm;
             } else {
