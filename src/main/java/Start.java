e3e5347910f43e394911ae3c97d75c275a7bead9
VIP <belevitin2010@mail.ru>
2022-03-16 13:17:22 +0300

    Metod return 2.0

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -9,32 +9,32 @@ public class Start {
     public static String findHighestGrossingFilm() {
         /** 
         * если заранее известно что будет лежать в переменной, дай ей осмысленное название
-        * например titanicFiml так же и с переменной income1 -> titcanicInocome
+        * например titanicFilm так же и с переменной income1 -> titanicIncome
         * и если значение изменяться не будет, сделай ее final
         * https://vertex-academy.com/tutorials/ru/modifikator-final/
         * так же и с остальными переменными
         */
-        String film1 = "Титаник";
-        int income1 = 2194;
+        String titanicFilm = "Титаник";
+       final int titanicIncome = 2194;
+int a;
+        String AvatarFilm = "Аватар";
+       final int AvatarIncome = 2810;
 
-        String film2 = "Аватар";
-        int income2 = 2810;
-
-        String film3 = "Тёмный рыцарь";
-        int income3 = 1084;
+        String DarkKnightFilm = "Тёмный рыцарь";
+       final int DarkKnightIncome = 1084;
 
         // Напишите реализацию нового метода ниже
-        if (income1 > income2) {
-            if (income1 > income3) {
-                return film1;
+        if (titanicIncome >AvatarIncome) {
+            if (AvatarIncome > DarkKnightIncome) {
+                return titanicFilm;
             } else {
-                return film2;
+                return AvatarFilm;
             }
         } else {
-            if (income2 > income3) {
-                return film2;
+            if (AvatarIncome > DarkKnightIncome) {
+                return AvatarFilm;
             } else {
-                return film3;
+                return DarkKnightFilm;
             }
         }
     }
