4b9a30d7f0c532c4fd8182f2970ac923391812ad
VIP <belevitin2010@mail.ru>
2022-03-16 18:47:44 +0300

    Fix made the expenses array a constant

--- src/main/java/DeclaringNewMethod.java
+++ src/main/java/DeclaringNewMethod.java
@@ -3,7 +3,7 @@ package main.java;
 public class DeclaringNewMethod {
     public static void main(String[] args) {
         double[] expenses = {1772.5, 367.0, 120.6, 2150.2, 874.0, 1.0, 1459.4};
-        double maxExpense = findMaxExpense(expenses); // Вызовите метод и присвойте maxExpense значение его результата
+        final double maxExpense = findMaxExpense(expenses); // Вызовите метод и присвойте maxExpense значение его результата
         System.out.println("Самая большая трата недели " + maxExpense);
     }
 
