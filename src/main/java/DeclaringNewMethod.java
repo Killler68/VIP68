d05cfe4437ef70ff67c4b34bb3fd59a325f83dab
VIP <belevitin2010@mail.ru>
2022-03-18 22:23:19 +0300

    Created worked with methods

--- src/main/java/DeclaringNewMethod.java
+++ src/main/java/DeclaringNewMethod.java
@@ -1,20 +1,58 @@
 package main.java;
-
+import java.util.Scanner;
 public class DeclaringNewMethod {
     public static void main(String[] args) {
-        double[] expenses = {1772.5, 367.0, 120.6, 2150.2, 874.0, 1.0, 1459.4};
-        final double maxExpense = findMaxExpense(expenses); // Вызовите метод и присвойте maxExpense значение его результата
-        System.out.println("Самая большая трата недели " + maxExpense);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Какую зарплату вы получите в этом месяце?");
+        int salary = scanner.nextInt();
+        System.out.println("Сколько планируете потратить на транспорт?");
+        int transportMoney = scanner.nextInt();
+        System.out.println("Сколько запланировано на супермаркеты?");
+        int foodMoney = scanner.nextInt();
+        System.out.println("Какую сумму хотите отложить?");
+        int savings = scanner.nextInt();
+
+        // Вызовите метод correctExpenses с правильными аргументами
+        correctExpenses(transportMoney, foodMoney, savings,salary);
+
+        // Напечатайте запланированные траты
+        System.out.println("Вы планировали потратить: транспорт — " + transportMoney + ", "
+                + "еда — " + foodMoney + ", "
+                + "сбережения — " + savings + ".");
+
     }
 
-    public static double findMaxExpense(double[] expenses) {
-        double maxExpense = expenses[0];
+    // Объявите метод correctExpenses
+    // Тело метода дано ниже
+    public static int correctExpenses(int transportMoney, int foodMoney, int savings, int salary) {
 
-        for (double expense : expenses) {
-            if (expense > maxExpense) {
-                maxExpense = expense;
+        int expensesSum = transportMoney + foodMoney + savings; // Считаем расходы
+        int leftMoney = salary - expensesSum;
+        if (expensesSum > salary) { // Проверяем, не превышают ли расходы зарплату
+            int lackMoney = expensesSum - salary; // Считаем, сколько не хватает
+            // Считаем излишек средств
+
+            // Пока не начнёт хватать денег на еду — сокращаем траты на 100 рублей
+            while ((salary - foodMoney) < transportMoney) {
+                foodMoney = foodMoney - 100;
             }
+            // Если не хватает денег на жизнь — не откладываем
+            if (transportMoney + foodMoney + savings > salary) {
+                savings = 0;
+            }
+
+            // Печатаем рекомендации
+            System.out.println("Придётся пересмотреть планы, вам не хватает " + lackMoney);
+            System.out.println("Рекомендуемые траты: "
+                    + "еда — " + foodMoney + ", "
+                    + "сбережения — " + savings + ".");
+        } else {
+            System.out.println("В этом месяце дебет с кредитом сошлись!");
+            System.out.println("Свободных средств " + leftMoney);
         }
-        return maxExpense;
+        return leftMoney;
     }
 }
+
+
+
