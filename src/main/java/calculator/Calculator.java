469a4528dbf97149e8e24fdb74adf6d97663c0df
VIP <belevitin2010@mail.ru>
2022-03-20 00:02:13 +0300

    Create calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,43 @@
 package main.java.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите оператор");
+        String operator = scanner.next();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+
+    }
+    public static  int calc ( String operator, int a, int b) {
+        int c;
+        switch (operator) {
+            case "+":
+                c = a + b;
+                break;
+            case "-":
+                c = a - b;
+                break;
+            case "*":
+                c = a * b;
+                break;
+            case "/":
+                c = a / b;
+                break;
+            default:
+                c = a = b;
+        }
+        return c;
     }
 }
+
+
+
+
+
+
