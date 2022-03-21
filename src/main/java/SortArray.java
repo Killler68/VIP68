a4621422e9be7eb2f4111986261b492b5ef718c0
VIP <belevitin2010@mail.ru>
2022-03-21 21:32:43 +0300

    Create sorting arrays

--- src/main/java/SortArray.java
+++ src/main/java/SortArray.java
@@ -18,25 +18,33 @@ public class SortArray {
     /**
      * константы с названиями городов
      * ExampleArrays.TAMBOV, ExampleArrays.MOSCOW, ExampleArrays.KIEV, ExampleArrays.TULA
-     *
+     * <p>
      * получение полей пользователя
      * user.getId();
      * user.getName();
      * user.getAge();
      * user.getCity();
-     *
+     * <p>
      * получение названия города пользователя
      * user.getCity().getName();
-     *
-     * */
+     */
 
     public static void main(String[] args) {
         fillCities();
         printCities();
+
     }
 
     private static void fillCities() {
 
+        for (User user : users) {
+            switch (user.getCity().getName()) { // User класс user объект getCity вызываю City
+                case ExampleArrays.TAMBOV -> tambov.add(user.getName());
+                case ExampleArrays.MOSCOW -> moscow.add(user.getName());
+                case ExampleArrays.KIEV -> kiev.add(user.getName());
+                case ExampleArrays.TULA -> tula.add(user.getName());
+            }
+        }
     }
 
     private static void printCities() {
