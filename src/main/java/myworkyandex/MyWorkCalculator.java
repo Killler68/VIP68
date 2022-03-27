f911afc36f9f9b37c724e692ab03ad27a0c0fd83
VIP <belevitin2010@mail.ru>
2022-03-27 18:58:44 +0300

    Fix created a class of operations and removed the infinite loop in the calculator

--- src/main/java/myworkyandex/MyWorkCalculator.java
+++ src/main/java/myworkyandex/MyWorkCalculator.java
@@ -6,17 +6,17 @@ public class MyWorkCalculator {
     public static Scanner scanner = new Scanner(System.in);
 
     public static double Calc() {
-        while (true) {
-            System.out.println("Введите первое число ");
-            double a = scanner.nextDouble();
-            System.out.println("Введите знак");
-            String operator = scanner.next();
-            System.out.println("Введите второе число ");
-            double b = scanner.nextDouble();
 
-            return Operation(operator, a, b);
-        }
+        System.out.println("Введите первое число ");
+        double a = scanner.nextDouble();
+        System.out.println("Введите знак");
+        String operator = scanner.next();
+        System.out.println("Введите второе число ");
+        double b = scanner.nextDouble();
+
+        return Operation(operator, a, b);
     }
+
 
     public static double Operation(String operator, double a, double b) {
 
