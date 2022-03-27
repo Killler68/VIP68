2bfb1468443c7a497bf4490a8a031766d7bf4101
VIP <belevitin2010@mail.ru>
2022-03-27 21:20:34 +0300

    Fix commit

--- src/main/java/practices/Human.java
+++ src/main/java/practices/Human.java
@@ -1,2 +1,27 @@
-package practices;public class Human {
-}
+package practices;
+
+public class Human {
+        String name; //name объекта
+        int age;
+
+        public void setName(String name) { // name метода
+            this.name = name; // this.name - name объекта, = name метода
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void getInfo() {
+            System.out.println(name + "," + age);
+        }
+    }
+
--- src/main/java/practices/WordThis.java
+++ src/main/java/practices/WordThis.java
@@ -1,2 +1,17 @@
-package practices;public class WordThis {
+package practices;
+
+public class WordThis {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setAge(18);
+        human.setName("Tom");
+        human.getInfo();
+        Human humans = new Human();
+        humans.setAge(25);
+        humans.setName("BOB");
+        humans.getInfo();
+    }
 }
+
+
+
