34daffe1add854ec1e6aa44239d6589fdbc4d304
VIP <belevitin2010@mail.ru>
2022-03-31 19:13:39 +0300

    Created overload

---
+++ src/main/java/practices/overload/Overload.java
@@ -0,0 +1,16 @@
+package practices.overload;
+
+public class Overload {
+
+    public Overload() {
+        System.out.println("hello");
+    }
+
+    public Overload(int a) {
+        System.out.println(a);
+    }
+
+    public Overload(String s) {
+        System.out.println(s);
+    }
+}
---
+++ src/main/java/practices/overload/TestOverload.java
@@ -0,0 +1,10 @@
+package practices.overload;
+
+public class TestOverload {
+    public static void main(String[] args) {
+        Overload overload = new Overload();
+        Overload overload1 = new Overload(2);
+        Overload overload2 = new Overload("may");
+
+    }
+}
