93ad703c98313d2f4a8e496eceeb4fbbc542af0c
VIP <belevitin2010@mail.ru>
2022-04-01 20:29:25 +0300

    Improved the guess the number game

--- src/main/java/practices/RandomNumber.java
+++ src/main/java/practices/RandomNumber.java
@@ -7,18 +7,20 @@ public class RandomNumber {
 
     public static void main(String[] args) {
         System.out.println("Ваша задача угадать число ");
-        int range = 20;
-        int number = (int) (Math.random() * range);
-        PlayLevel(range, number);
+        for (int i = 10; i <= 30; i += 10) PlayLevel(i);
+        System.out.println("Вы выиграли!!!");
         scanner.close();
     }
-    private static void PlayLevel(int range, int number) {
+
+    private static void PlayLevel(int range) {
+        int number = (int) (Math.random() * range);
+
         while (true) {
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
             if (input_number == number) {
                 System.out.println("Поздравляю вы угадали");
-            break;
+                break;
             } else if (input_number > number) {
                 System.out.println("Загаданное число меньше");
             } else if (input_number < number) {
